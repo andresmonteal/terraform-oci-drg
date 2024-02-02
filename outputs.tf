e73675a750cbbe1c2fb627fede50f426f9c45df9
Andres Montealegre <andres.montealegre@oracle.com>
2024-02-02 16:23:32 -0500

    fix: adds comparment id output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "Resource id"
   value       = oci_core_drg.main.id
 }
+
+output "compartment_id" {
+  description = "Resource compartment_id id"
+  value       = oci_core_drg.main.compartment_id
+}
